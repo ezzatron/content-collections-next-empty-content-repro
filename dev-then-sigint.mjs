5341319b0dacb3a462a3c7a7b5fb10ad8f5d9c35
Erin Millard <contact@erin.id.au>
2025-02-20 14:59:15 +1000

    Add an NPM script to run then kill the dev server

---
+++ dev-then-sigint.mjs
@@ -0,0 +1,12 @@
+
+import { exec } from 'node:child_process';
+
+const DELAY = 5_000;
+
+const dev = exec('npm run dev');
+dev.stdout.pipe(process.stdout);
+dev.stderr.pipe(process.stderr);
+
+setTimeout(() => {
+  dev.kill('SIGINT');
+}, DELAY);
