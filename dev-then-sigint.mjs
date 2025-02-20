5d011312ebda8c761ba0913f1046827c438c926b
Erin Millard <contact@erin.id.au>
2025-02-20 15:14:46 +1000

    Try spawning a detached process

--- dev-then-sigint.mjs
+++ dev-then-sigint.mjs
@@ -1,13 +1,10 @@
-
-import { exec } from 'node:child_process';
+import { spawn } from 'node:child_process';
 
 const DELAY = 5_000;
 
-const dev = exec('npm run dev');
-dev.stdout.pipe(process.stdout);
-dev.stderr.pipe(process.stderr);
+const dev = spawn('npm', ['run', 'dev'], { stdio: 'inherit', detached: true });
 
 setTimeout(() => {
   console.log("Sending SIGINT...");
-  dev.kill('SIGINT');
+  process.kill(-dev.pid, 'SIGINT');
 }, DELAY);
